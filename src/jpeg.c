4d4bd40572a86ff51c99f1d4408993693001d337
Zhang Boyang <zhangboyang.id@gmail.com>
2015-02-03 16:13:57 +0800

    do not display error message

--- src/jpeg.c
+++ src/jpeg.c
@@ -22,7 +22,6 @@ typedef struct my_error_mgr * my_error_ptr;
 static void my_error_exit(j_common_ptr cinfo)
 {
     my_error_ptr myerr = (my_error_ptr) cinfo->err;
-    (*cinfo->err->output_message) (cinfo);
     longjmp(myerr->setjmp_buffer, 1);
 }
 
